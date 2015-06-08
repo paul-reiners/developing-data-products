4714f9bc401627bc76c4381527445cf6ff4ac0ab
Paul Reiners <paul.reiners@gmail.com>
2015-06-08 13:49:45 -0500

    Adding diabetes computation

--- app/ui.R
+++ app/ui.R
@@ -1,10 +1,19 @@
 library(shiny)
-shinyUI(pageWithSidebar(
-  headerPanel("Data science FTW!"),
-  sidebarPanel(
-    h3('Sidebar text')
-  ),
-  mainPanel(
-    h3('Main Panel text')
+
+shinyUI(
+  pageWithSidebar(
+    # Application title
+    headerPanel("Diabetes prediction"),
+    sidebarPanel(
+      numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
+      submitButton('Submit')
+    ),
+    mainPanel(
+      h3('Results of prediction'),
+      h4('You entered'),
+      verbatimTextOutput("inputValue"),
+      h4('Which resulted in a prediction of '),
+      verbatimTextOutput("prediction")
+    )
   )
-))
+)
