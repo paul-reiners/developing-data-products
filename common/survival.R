0f50a6e7dd5ab59a023921f01cf843398b2a3e8e
Paul Reiners <paul.reiners@gmail.com>
2015-06-12 16:29:57 -0500

    Pulled out a function.

---
+++ common/survival.R
@@ -0,0 +1,10 @@
+getSurvivalTable <- function() {
+  data = read.csv("../data/train.csv")
+  data$SurvivedStr <- ifelse(data$Survived == 0, "perished", "survived")
+  data$ChildOrAdult <- ifelse(data$Age < 18, "child", "adult")
+  sex.age.table <- table(data$Sex, data$ChildOrAdult, data$SurvivedStr)
+  survival.table <- 
+    sex.age.table[,,2] / (sex.age.table[,, 1] + sex.age.table[,,2])
+  
+  return(survival.table)
+}
